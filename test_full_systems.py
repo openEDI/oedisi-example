04436da772563953283186320f277071480005fa
Joseph McKinsey <joseph.mckinsey@nrel.gov>
2022-08-26 11:14:47 -0600

    Use gadal namespace in test_full_systems

--- test_full_systems.py
+++ test_full_systems.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # Load component framework
-from componentframework.basic_component import component_from_json
-from componentframework.system_configuration import (
+from gadal.componentframework.basic_component import component_from_json
+from gadal.componentframework.system_configuration import (
     generate_runner_config,
     WiringDiagram,
 )
