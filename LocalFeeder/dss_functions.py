dba119d922121e8d33f86241455fe310a28552ae
jliang9 <101606229+jliang9@users.noreply.github.com>
2022-09-16 11:46:28 -0600

    Exclude Q in Ymatrix

--- LocalFeeder/dss_functions.py
+++ LocalFeeder/dss_functions.py
@@ -39,7 +39,7 @@ def get_y_matrix_file(dss):
     dss.run_command('batchedit load..* enabled=false')
     dss.run_command('batchedit generator..* enabled=false')
     dss.run_command('batchedit pvsystem..* enabled=false')
-    dss.run_command('Batchedit Capacitor..* enabled=yes')
+    dss.run_command('Batchedit Capacitor..* enabled=false')
     dss.run_command('batchedit storage..* enabled=false')
     dss.run_command('CalcVoltageBases')
     dss.run_command('set maxiterations=20')
