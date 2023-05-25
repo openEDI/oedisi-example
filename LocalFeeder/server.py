d3ed3006d74b53cd6ae8110905e8c423fd8fb063
Latif <aadil.latif@gmail.com>
2023-05-25 08:41:19 -0600

    asdsf

--- LocalFeeder/server.py
+++ LocalFeeder/server.py
@@ -1,11 +1,16 @@
 from gadal.gadal_types.common import BrokerConfig
-from fastapi import FastAPI, BackgroundTasks
+from fastapi import FastAPI, BackgroundTasks, UploadFile
+from fastapi.exceptions import HTTPException
 from sender_cosim import run_simulator
+import zipfile
 import uvicorn
 import socket
 import sys
 
 app = FastAPI()
+
+
+
 
 @app.get("/")
 def read_root():
@@ -13,6 +18,22 @@ def read_root():
     host_ip = socket.gethostbyname(hostname)
     return {"hostname": hostname, "host ip": host_ip}
 
+@app.post("/model/")
+async def upload_model(file:UploadFile):
+    try:
+        data = file.file.read()
+        if not file.filename.endswith(".zip"):
+            HTTPException(400, "Invalid file type. Only zipped opendss models are accepted.")
+        
+        with open(file.filename, "wb") as f:
+            f.write(data)
+            
+        with zipfile.ZipFile(file.filename, 'r') as zip_ref:
+            zip_ref.extractall("./")
+        
+        return {"reply": "success", "error": False, "action": f"File uploaded to server: {file.filename}" }
+    except Exception as e:
+        return {"reply": str(e), "error": True}
 
 @app.post("/run/")
 async def run_feeder(broker_config:BrokerConfig, background_tasks: BackgroundTasks):
